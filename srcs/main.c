aba86eeb56bfbc2ed3dcd3eb8ad866a39d204e4e
Arthur BONNECARRERE <abonneca@e1r4p10.42.fr>
2016-11-07 21:15:17 +0100

    "ok"

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: abonneca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 20:41:27 by abonneca          #+#    #+#             */
-/*   Updated: 2016/11/07 20:50:43 by abonneca         ###   ########.fr       */
+/*   Updated: 2016/11/07 21:13:22 by abonneca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 
 int	main(int ac, char **ar)
 {
+	int fd;
+
+	fd = 0;
 	if (ac == 2)
 	{
-		ft_putstr(ar[1]);
+		if (!(fd = open(ar[1], O_RDWR)))
+			ft_putstr(ar[1]);
+		if (close(ar[1]) == -1)
+			ft_putstr("close error ()");
 	}
 	else
 	{
