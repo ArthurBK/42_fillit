010904b6d81421b99d3c815817fc9874f6acddb7
Arthur BONNECARRERE <abonneca@e2r8p3.42.fr>
2016-11-09 15:38:26 +0100

    "pouet"

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: abonneca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/08 17:38:40 by abonneca          #+#    #+#             */
-/*   Updated: 2016/11/09 13:26:14 by abonneca         ###   ########.fr       */
+/*   Updated: 2016/11/09 13:49:09 by abonneca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@
 
 # define NUMBER_OF_PATTERNS 19
 
+#include <stdio.h>
+#include <fcntl.h>
 typedef struct	s_tetri
 {
 	char	*pattern;
 	int		offset;
 }				t_tetri;
 
-int    ft_check_pattern(char *str);
+int		ft_check_main(char *str);
+int		ft_check_pattern(char *str);
 
 #endif
